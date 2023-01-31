6ce1b45ae32270bea78c1f00db341aa3ffe44c86
michellewong793 <michellewong793@gmail.com>
2023-01-30 21:30:41 -0800

    Add git ignore and new link

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ export default function Index() {
         <h1> Queer Art Faire 2023 Schedule</h1>
         <h2>Spring Fair: March 11, 2023</h2>
         <a href="/march-11-vendors"> Spring Fair Vendor List </a>
+        <p><a href="https://www.eventbrite.com/e/2023-queer-art-faire-tickets-499055427217?aff=efbneb">Tickets to Event</a></p>
         <p> Location: Sports Basement Stonestown </p>
         <h2> Summer Fair: June 17, 2023</h2>
         <p> Location: Sports Basement Presidio </p>
