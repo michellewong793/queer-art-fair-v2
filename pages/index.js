d77810a5c9bd3f1aa7da809651cb70bda938b9b9
michellewong793 <michellewong793@gmail.com>
2023-03-22 12:04:38 -0700

    push new copy

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,10 @@ export default function Index() {
       <Layout />
       <div style={Styles.content}>
         <h1> 😱 What is Queer Art Faire? </h1>
-        <p>We are a community of artists, makers and vendors hosting inclusive and accessible art and maker fairs in San Francisco.
+        <p>We are a community hosting art fairs in San Francisco for <strong>marginalized</strong> and queer folks who have not had a space to share their creations before. 
+          We are focused on creating spaces for people whose first language is not English, people of color, people who have not been in the game for very long, or have little to no experience vending or sharing their art. 
+          We aim to create a welcoming environment for people who face imposter syndrome about sharing their art to begin their journeys. 
+          We are a completely volunteer-run organization running on donations. If you want to support us, we will be offering collectible merch at our fairs, and we receive donations on Venmo: @queerartfair.
           </p>
 
 
@@ -27,8 +30,8 @@ export default function Index() {
 
 
       <Spacer height={2}/>
-      <h2>How can I support Queer Art Faire?</h2>
-        <p>If you want to support our mission to provide accessible and inclusive art fairs for all in San Francisco, please feel free to donate more below to help us cover the costs of making QAF happen. Our hearts will be very warm. We are in the process of filing for 501c3 status.</p>
+      <h2>How can I support?</h2>
+        <p>If you want to support our mission, you can volunteer or donate below to help us cover the costs of making QAF happen. Our hearts will be very warm. We are in the process of filing for 501c3 status.</p>
         <p>We are also looking for volunteer DJs, social media managers, day-of setup and tabling assistants. If you are interested, please get in touch <a href="mailto:michellurito@gmail.com">here.</a></p>
        <Spacer height={1}/>
          <a style={Styles.link} href="https://www.gofundme.com/f/queer-art-faire?utm_source=facebook&utm_medium=social&utm_campaign=p_cf%20share-flow-1&fbclid=IwAR2AgoudjzRzqhv20nRF1eQuLQjybLwB8aQxPMbORKvZZnJOCf2pTpFBTNQ"
@@ -38,8 +41,8 @@ export default function Index() {
         <br/>
        
       <br/>
-      <h2>Apply to share your art / craft / creative passion!</h2>
-        <h3> <a href="https://forms.gle/Ada3uBFQoxUJk1ka7">Fill out an application here!</a> </h3>
+      <h2>Apply to share your craft in a welcoming space for folks new to vending!</h2>
+        <h3> <a href="https://forms.gle/Ada3uBFQoxUJk1ka7">Fill out an application here</a> </h3>
         <p>Our upcoming event on June 17 is currently accepting applications. We welcome artists of all trades. We will have a $5 early bird vendor fee if you apply before April 17. After April 17, the vendor fee will increase to $15.</p>
 
 
