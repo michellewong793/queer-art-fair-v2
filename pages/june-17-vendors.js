3333681a5366005dfb711718f25d9a3bb866e3a7
michellewong793 <michellewong793@gmail.com>
2023-04-17 18:25:55 -0700

    new edits

--- pages/june-17-vendors.js
+++ pages/june-17-vendors.js
@@ -9,98 +9,119 @@ export default function June17Vendors() {
       <div style={Styles.content}>
         <h1> Summer Queer Art Fair Vendors - June 17 2023 </h1>
         <h2> Location: Pebble Bed 1417 15th St, San Francisco, CA 94103  </h2>
-        <h3> Time: 1am-6pm </h3>
+        <h3> Time: 1pm-6pm </h3>
+        <p> $5 donation encouraged for attendees</p>
 
-    
- <p> 1. <a href="https://www.instagram.com/partyofimmortals/">Hyazinth
- </a>- <a href="https://www.hyazinths.com">multimedia zines oriented around queer mythology</a></p>
- <p> 2. <a href="https://www.instagram.com/perfectlypinay/">Perfectly Pinay
+<ol>
+    <li><p> <a href="https://www.instagram.com/partyofimmortals/">Hyazinth
+    </a>- <a href="https://www.hyazinths.com">multimedia zines oriented around queer mythology</a></p>
+    </li>
+    <li><p> <a href="https://www.instagram.com/perfectlypinay/">Perfectly Pinay
  </a>- <a href="https://www.perfectlypinay.com">stationery, drinkware, and apparel that promote mental wellness, identity, and Filipinx culture
 </a></p> 
-<p> 3. <a href="https://www.instagram.com/jun333designs/">Jun333Designs
+</li>
+<li><p> <a href="https://www.instagram.com/herbalcommunion/">Herbal Communion
+ </a>- <a href="https://www.herbalcommunion.com">handcrafted herbal extracts and skincare products
+</a></p> 
+</li>
+
+
+<li><p><a href="https://www.instagram.com/jun333designs/">Jun333Designs
  </a>- handmade chainmail jewelry</p>
-<p> 4. <a href="https://www.instagram.com/babygirl.inc.xo/">Babygirl Inc.
+ </li>
+ <li><p><a href="https://www.instagram.com/babygirl.inc.xo/">Babygirl Inc.
  </a>-jewelry, collars, cuffs, and chain chest harnesses
 </p>
-<p> 5. <a href="https://www.instagram.com/gracelyn.__/">Grxcelyn. Arts - Grace Birnam
+</li>
+<li><p><a href="https://www.instagram.com/gracelyn.__/">Grxcelyn. Arts - Grace Birnam
  </a>- <a href="https://grxcelynarts.bigcartel.com/">scrapbook pages, scrapbook page kits, stickers of digital art, and digital art prints 
 </a></p>
-<p> 6. <a href="https://www.instagram.com/Jsiuzmuidzinas/">Heir Boots/ Jsiuzmuidzinas
+</li>
+  <li><p><a href="https://www.instagram.com/Jsiuzmuidzinas/">Heir Boots/ Jsiuzmuidzinas
  </a>-original paintings,  t-shirts, and prints </p>
-<p> 7. <a href="https://www.instagram.com/ivanamakesthings/">Ivana Makes Things
+ </li>
+  <li><p><a href="https://www.instagram.com/ivanamakesthings/">Ivana Makes Things
  </a>-witty stickers</p>
-<p> 8. <a href="https://www.instagram.com/CodysTreats
+ </li>
+  <li><p><a href="https://www.instagram.com/CodysTreats
 /">Cody’s Cookies and Treats
  </a>- dehydrated cookies, treats, and chews for dogs and cats 
 </p>
-<p> 9. <a href="https://www.instagram.com/msuarezthai/">M. Suarez-Thai
+</li>
+  <li><p><a href="https://www.instagram.com/msuarezthai/">M. Suarez-Thai
  </a>- <a href="https://msuarezthai.bigcartel.com/">comics, zines, stickers, and prints
 </a></p>
-<p>10. <a href="https://www.instagram.com/A.j.k.artist/">Alexa Kern
+</li>
+  <li><p><a href="https://www.instagram.com/A.j.k.artist/">Alexa Kern
  </a>- fun, psychedelic art</p>
-<p>11. <a href="https://www.instagram.com/studio.mitchii/">Studio Mitchii
+ </li>
+  <li><p><a href="https://www.instagram.com/studio.mitchii/">Studio Mitchii
  </a>-ceramic items such as vases, pots, and mugs</p>
-<p> 12. <a href="https://www.instagram.com/by.evey/">Evelyn
+ </li>
+  <li><p> <a href="https://www.instagram.com/by.evey/">Evelyn
  </a>- crochet items, handmade earrings, and some vintage clothes</p>
-<p> 13. <a href="https://www.instagram.com/inky.fingers.printshop/">Inky Fingers Print Shop
+ </li>
+  <li><p> <a href="https://www.instagram.com/inky.fingers.printshop/">Inky Fingers Print Shop
  </a>- Prints, Textiles, Patches</p>
-<p> 14. <a href="https://www.instagram.com/tajiinmamii/">TajiinMamii
+ </li>
+  <li><p> <a href="https://www.instagram.com/tajiinmamii/">TajiinMamii
  </a>- queer Latina artist who creates earrings, necklaces, and phone charms </p>
-<p> 15. <a href="https://www.instagram.com/sproessling.magick
+ </li>
+  <li><p> <a href="https://www.instagram.com/sproessling.magick
 /">Sproessling Magick
  </a>-Cute stickers, magickal art and crystals ! 
- </p>
- <p> 16. <a href="https://www.instagram.com/asho.prints
+ </p></li>
+  <li> <p> <a href="https://www.instagram.com/asho.prints
 /">asho.prints
  </a>-lino cut prints and cards inspired by nature, queer identity, and the process of finding patience through the flow of printmaking
- </p>
- <p> 17.<a href="https://www.instagram.com/marionvailed
+ </p></li>
+   <li><p><a href="https://www.instagram.com/marionvailed
 /"> marionvailed
- </a>- toy keychains and apparel
- </p>
- <p> 18.<a href="https://www.instagram.com/ai.archives
+ </a>- toy keychains and apparel</p>
+ </li>
+  <li> <p> <a href="https://www.instagram.com/ai.archives
 /"> ai.archives
  </a>- upcycled and vintage clothing
  </p>
- <p> 19. v che
+ </li>
+   <li><p>  v che
 - prints, art, ceramics, and more!
- </p>
- <p> 20.<a href="https://www.instagram.com/reikii_love
+ </p></li>
+   <li><p> <a href="https://www.instagram.com/reikii_love
 /"> Reiki Love
  </a> - intuitive tarot + oracle readings to the collective 
- </p>
- <p> 21.<a href="https://www.instagram.com/wolfbabeshop
+ </p></li>
+   <li><p> <a href="https://www.instagram.com/wolfbabeshop
 /"> Wolf Babe Shop polymer clay jewelry
  </a> - polymer clay jewelry mostly tiny bongs and pipes and little boobies :)
- </p>
- <p> 22.<a href="https://www.instagram.com/mew.doodles
+ </p></li>
+  <li> <p> <a href="https://www.instagram.com/mew.doodles
 /"> Mew Doodles
  </a> - cute art, stationery, and apparel inspired by hedgehogs, plants, & asian food
- </p>
- <p> 22.<a href="https://evy.garden/ 
-/"> evy
+ </p></li>
+  <li> <p> <a href="https://evy.garden/"> evy
  </a> - zines and collage art (some originals, some prints)
- </p>
- <p> 23.<a href="https://www.instagram.com/moodyzillenial
+ </p></li>
+  <li> <p> <a href="https://www.instagram.com/moodyzillenial
 /">moody zillenial
  </a> - a variety of rave hair accessories both sourced and handmade 
- </p>
- <p> 24.<a href="https://www.instagram.com/woodymakesthings
+ </p></li>
+   <li><p> <a href="https://www.instagram.com/woodymakesthings
 /"> woodymakesthings
  </a> - playful illustrations, stickers, and handmade ceramics
- </p>
- <p> 25.<a href="https://www.instagram.com/New_stitching
+ </p></li>
+   <li><p> <a href="https://www.instagram.com/New_stitching
 /"> New_stitching
  </a> - handmade embroidered bags
- </p>
- <p> 26.<a href="https://www.instagram.com/softgentlepancake
+ </p></li>
+   <li><p> <a href="https://www.instagram.com/softgentlepancake
 /"> Soft Gentle Pancake
  </a> - zines and comics about navigating sex work, grief, queer sex, and absurdism
- </p>
- <p> 27.<a href="https://www.instagram.com/astuartdesign
+ </p></li>
+   <li><p> <a href="https://www.instagram.com/astuartdesign
 /"> Anna Stuart Design
  </a> - risograph prints and stickers
- </p>
+ </p></li>
      
      
      <br/>
@@ -113,7 +134,7 @@ export default function June17Vendors() {
  </a> - coffee and drinks 
  </p>
      
-      
+      </ol>
       </div>
     </div>
   );
