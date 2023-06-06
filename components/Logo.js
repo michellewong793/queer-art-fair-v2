ee786764de5d6075b435b0ef625ee6c08c800e27
Elaine Demetrion <esdemetrion@gmail.com>
2023-06-06 10:18:02 -0700

    Creates centered logo component for header

---
+++ components/Logo.js
@@ -0,0 +1,19 @@
+import Styles from './Theme.js';
+
+const Logo = (props) => {
+    let divStyle = {
+        display: 'flex',
+        width: '100%',
+    }
+    let imgStyle = {
+        margin: 'auto',
+        width: '240px',
+    }
+    return (
+        <div style={divStyle}>
+            <img style={imgStyle} src='./QAFLogoOrangePuffy.png'></img>
+        </div>
+    )
+}
+
+export default Logo;
