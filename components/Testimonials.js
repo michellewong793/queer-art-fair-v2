1ecbae30dc010febebbc0be2d2ee2f106d76cf44
Elaine Demetrion <esdemetrion@gmail.com>
2023-06-12 12:23:19 -0700

    A list of quotes from the google form responses

---
+++ components/Testimonials.js
@@ -0,0 +1,50 @@
+const Testimonials = [
+    {
+        testimonial: "I really loved meeting the other vendors! I traded a pair of earrings for a hair clip from frootyshop, and traded another pair of earrings for a tattoo from flashedbystace. It was a beautiful day of sharing art with radical queer artists <3",
+        source: "junenelsonx"
+    },
+    {
+        testimonial: "The crowd was so nice! Even those who didn't purchase anything, they'd still showed appreciation for my work and that felt really good :) Meeting other artists were cool too - I didn't study art in school and so my community of art friends is mainly whoever I meet online - it was so nice to be among other artists in person!",
+        source: "Anonymous"
+    },
+    {
+        testimonial: "It was nice reconnecting with people from my high school. I did not know that they also have art/craft hobbies too! I was also able to connect with a customer because they saw that I put \"K-Pop Lover\" on my IG bio. I asked them if they liked NewJeans (a newer kpop group, who has gained a lot of popularity), and they showed me their k-pop photocards from their bag! I love how we were able to connect on similar interests other than stationery too! Another cute story was when a customer came up to my shop, and they were already into sticker planning! The customer called me \"Sticker Costco\".",
+        source: "annalam218"
+    },
+    {
+        testimonial: "I absolutely loved the vibes! I think that everyone was amazing and talented, and there was great diversity in the types of art and vendors who were there. I was impressed by the amount of people who showed up, and I loved connecting with both vendors and just folks stopping by. I think the organizers did an incredible job, and were very responsive. The entire process was easy and simple. In particular I found the ability to hear from other artists about where else they vend and swap tips, etc was incredibly valuable as someone who's just starting out.",
+        source: "ashoka.alvarez"
+    },
+    {
+        testimonial: "A friend we made at a karaoke night in SJ showed up to the fair to support. She is new to town in SJ and I think she was really happy to find cool artists/friends. I'm going to hang out with her soon! yay new friends!",
+        source: "Anonymous",
+    },
+    // {
+    //     testimonial: "Community and talking to other artist were really amazing. The volunteers and organizers were all super helpful, kind, and accomodating",
+    //     source: "inkyfingersprintshop",
+    // },
+    
+    // {
+    //     testimonial: "Getting my first sale before the fair started because Mission Tarot wanted a tip jar that he could also drink out of. Being told that since I do pottery and baking that I could just called myself \"Twice Baked\" and that I must be \"great with my hands\". Seeing friends that I haven't seen in ages come and support me and my work",
+    //     source: "andrewjtolentino"
+        
+    // },
+    
+    // {
+    //     testimonial: "The community was so kind and it was such a nice surprise to connect with old high school friends (vending and those visiting). I also met alumni from my high school and it was cool to see everyone's passions!",
+    //     source: "sally.zhao"
+    // },
+    
+    
+    // {
+    //     testimonial: "it was laid back and everyone was very friendly.  Also really liked the space",
+    //     source: "dirtboyceramics"
+    // },
+    
+    // {
+    //     testimonial: "It was really nice to discover that everyone around me also went to Lowell, that there are still some of us still in the city, trying to do interesting, community-oriented projects outside of tech.",
+    //     source: "JUSTINCAMERONLAI"
+    // }
+]
+
+export default Testimonials;
