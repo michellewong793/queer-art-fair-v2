ebce3bd6dc6cb7567e6b5050abbcd3bf824a4e10
Elaine Demetrion <esdemetrion@gmail.com>
2023-06-12 14:23:25 -0700

    Deletes styling from js file

--- components/Testimonial.js
+++ components/Testimonial.js
@@ -1,29 +1,11 @@
+import Styles from './Testimonial.module.css'
 const Testimonial = (Props) => {
-    let Styles = {
-        container: {
-            display: 'flex',
-            flexDirection: 'row',
-            width: '100%',
-            justifyContent: 'center'
-        },
-        textContainer: {
-            width: '60%',
-            padding: '10px',
-            margin: '10px',
-            border: '1px solid #FF4C4C',
-            backgroundColor: '#ffbec0',
-            boxShadow: '-1px 2px 5px 0px rgba(0, 0, 0, 0.5);'
-        },
-        source: {
-            textAlign: 'center'
-        }
-    }
 
     return (
-        <div style={Styles.container}>
-            <div style={Styles.textContainer}>
+        <div className={Styles.container}>
+            <div className={Styles.textContainer}>
                 <p>"<em>{Props.testimonial}</em>"</p>
-                <p style={Styles.source}>-{Props.source}</p>
+                <p className={Styles.source}>-{Props.source}</p>
             </div>
         </div>
     )
