167bdf52b23998b227d6837222fede671ff7f0a5
CDarling25 <135234079+CDarling25@users.noreply.github.com>
2023-06-14 09:15:38 -0400

    removed hearts from mobile nav bar and fixed bug so that the hamburger icon remains at the top of nav options

--- components/HeaderDecoration.js
+++ components/HeaderDecoration.js
@@ -10,24 +10,27 @@ const HeaderDecoration = (props) => {
 
     const [showNav, setShowNav] = useState(false);
 
+    
+
 
     return (
         <div>
             <div className ={headerStyle.headerFill}>
+                <button className = {style.button} onClick = {() => setShowNav(!showNav)}></button>
                 <div className = {style.mobileContainer} style = {{display: showNav ? "flex" : "none"}}> 
                     <a  href = "/" className = {currentRoute === '/' ? style.active : style.nonActive}>
                         ABOUT
                     </a>
-                    <img className = {style.image} src = '/heart.png'/>
+                    
                     <a  href = "/events" className = {currentRoute === '/events' ? style.active : style.nonActive}>
                         EVENTS
                     </a>
-                    <img className = {style.image} src = '/heart.png'/>
+                    
                     <a  className = {style.nonActive} href = "https://www.gofundme.com/f/queer-art-faire">
                         DONATE
                     </a>
                 </div>
-                <button className = {style.button} onClick = {() => setShowNav(!showNav)}></button>
+                
             </div>
             <div className = {headerStyle.header}></div>
         </div>
