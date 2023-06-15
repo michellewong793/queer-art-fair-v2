3c87d4d8df373245ffb55d46c3ac5934bc013ab1
Elaine Demetrion <esdemetrion@gmail.com>
2023-06-14 17:06:12 -0700

    Puts all footer spacing in multiples of 8

--- components/Footer.js
+++ components/Footer.js
@@ -1,66 +1,34 @@
-import Link from "next/link"
 import Button from "./Button"
+import Styles from './Footer.module.css'
 
 const Footer = (props) => {
-    let styles = {
-        footerDecor: {
-            width: '100%',
-            height: '74px',
-            backgroundImage: "url('FooterEllipse.svg')",
-            backgroundRepeat: 'repeat-x',
-            filter: 'drop-shadow(0 -10px 5px rgba(0, 0, 0, 0.2))',
-            marginBottom: '-1px', // There is a space at the bottom for some reason
-            marginTop: '40px',
-        },
-
-        footer: {
-            width: '100%',
-            backgroundColor: '#FFF9ED',
-            paddingTop: '20px',
-            paddingBottom: '40px',
-        },
-
-        centerText: {
-            textAlign: 'center',
-        },
-
-        buttonContainer: {
-            height: 125,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'space-around',
-        },
-    }
-
     return (
         <div>
-            <div style={styles.footerDecor}></div>
-            <div style={styles.footer}>
-                <h3 style={styles.centerText}>Get updates from us!</h3>
+            <div className={Styles.footerDecor}></div>
+            <div className={Styles.footer}>
+                <h3 className={Styles.centerText}>Get updates from us!</h3>
 
-                <div style = {styles.buttonContainer}>
-                    <Button text='Join our email newsletter' 
+                <div className={Styles.buttonContainer}>
+                    <Button text='Join our email list' 
                         backgroundColor="white"
                         textColor="#002809"
                         borderColor='#FF4C4C'
                         hoverBackgroundColor="#002809"
                         hoverTextColor="white"
-                        width='300px'
                         url='https://subscribepage.io/queerartfaire'
                     />
-
+                </div>
+                <div className={Styles.buttonContainer}>    
                     <Button text='Follow us on Instagram' 
                         backgroundColor="white" 
                         textColor="#002809" 
                         borderColor='#FF4C4C'
                         hoverBackgroundColor="#002809"
                         hoverTextColor="white"
-                        width='300px'
                         url='https://www.instagram.com/queerartfairsf/'
                         />
-
                 </div>
+
             </div>
         </div>
     )
