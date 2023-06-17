eecd6f2caf933833947ea638d2ebfe3f9664705e
Elaine Demetrion <esdemetrion@gmail.com>
2023-06-16 23:35:18 -0700

    Increases space between testimonials, changes font to helvetica

--- components/Testimonial.js
+++ components/Testimonial.js
@@ -2,11 +2,9 @@ import Styles from './Testimonial.module.css'
 const Testimonial = (Props) => {
 
     return (
-        <div className={Styles.container}>
-            <div className={Styles.textContainer}>
-                <p>"<em>{Props.testimonial}</em>"</p>
-                <p className={Styles.source}>-{Props.source}</p>
-            </div>
+        <div className={Styles.textContainer}>
+            <p>"{Props.testimonial}"</p>
+            <p className={Styles.source}>-{Props.source}</p>
         </div>
     )
 }
