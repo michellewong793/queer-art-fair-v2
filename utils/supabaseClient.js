d00e24e0f162657c307b34023db4849a2c7b53e7
Elaine Demetrion <esdemetrion@gmail.com>
2023-06-28 13:22:40 -0700

    Uses cookies instead of local storage

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -1,7 +1,7 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseSecretKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-const supabaseClient = createClient(supabaseUrl, supabaseSecretKey);
+const supabaseClient = createClientComponentClient(supabaseUrl, supabaseSecretKey);
 
 export default supabaseClient;
