9e58bd6c92c82f6ccfe272a53d959c98abb43177
Elaine Demetrion <esdemetrion@gmail.com>
2023-06-28 16:49:13 -0700

    Deletes unused files and puts signIn file in login folder

--- utils/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseSecretKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-const supabaseClient = createClientComponentClient(supabaseUrl, supabaseSecretKey);
-
-export default supabaseClient;
