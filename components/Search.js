aee7cf510d210a1facebd550667fab8ccd36b716
CDarling25 <135234079+CDarling25@users.noreply.github.com>
2023-06-29 14:41:45 -0500

    I finished the function that creates a list of words that match the user input

--- components/Search.js
+++ components/Search.js
@@ -3,15 +3,26 @@ import React, {useState} from 'react';
 
 let products = ["pants", "stickers", "cards", "postcards", "bracelets", "necklaces", "paintings", "pots", "knitting", "sweaters", "clothes", "cloth", "prints", "earrings", "stamps"];
 
- function autocompleteSearch(products, inp) {
+
+
+ function autocompleteSearch(products) {
+    //gets the input from the text input field 
+    let inp = document.getElementById("searchTerm").value;
+    console.log(inp);
+
+    //initializes an empty array for the autocomplete suggestions
     let suggestions = [];
+
     if (inp == "") {
         console.log("nothing");
+        return [];
     }
     else {
+        //tests each word in the array against the user input if it matches that word is added to the list of suggestions
         for (let i = 0; i < products.length; i++){
             let wordToTest = products[i];
-            let inputReg = RegExp(inp);
+            let userInput = "^" + inp; 
+            let inputReg = RegExp(userInput);
             let result = inputReg.test(wordToTest);
             if (result == true) {
                 suggestions.push(wordToTest);
@@ -19,25 +30,30 @@ let products = ["pants", "stickers", "cards", "postcards", "bracelets", "necklac
         }
         console.log(suggestions);
     }
+
 } 
 
 export default function Search() {
+    const [isempty, setIsEmpty] = useState(true);
 
     let products = ["pants", "stickers", "cards", "postcards", "bracelets", "necklaces", "paintings", "pots", "knitting", "sweaters", "clothes", "cloth", "prints", "earrings", "stamps"]
 
-    
-
     return(
         <div className = {style.container}>
-            <form autocomplete = "off">
+            <form autocomplete = "off" >
                 <div class = "autocomplete" >
+                    <label htmlFor='searchTerm'></label>
                     <input 
                         className = {style.inputText} 
-                        onKeyUp = {() => autocompleteSearch(products, )}
-                        id = "searchTerm" 
+                        onKeyUp = {() => autocompleteSearch(products)}
+                        name = "searchTerm" 
+                        id = "searchTerm"
                         type = "text" 
                         placeholder='search for products...'
                     />
+                </div>
+                <div>
+                    //put all of the suggestions here make it display or not based on if it is empty
                 </div>
                 <input className = {style.inputSubmit} type = "submit"/>
             </form>
