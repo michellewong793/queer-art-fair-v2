aa0ba5f11f8b57d6c9905c2f5266322df4322473
Elaine Demetrion <esdemetrion@gmail.com>
2023-06-30 13:28:46 -0700

    Fixes null session bug on new item page

--- app/new-item/page.jsx
+++ app/new-item/page.jsx
@@ -1,16 +1,13 @@
 // the form to add a new item
 
 import ItemForm from './item-form'
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 
 export default async function Page() {
-    const supabase = createClientComponentClient({ cookies })
+    const supabase = createServerComponentClient({ cookies })
 
     const { data: { session }, } = await supabase.auth.getSession()
-    console.log('cookies: ', { cookies })
-    console.log('session: ', session)
-    console.log('user: ', session?.user)
 
     return (
         <ItemForm session={session}/>
