05716d2eccd50344e8c23fadb7fd5625bdb998ef
Elaine Demetrion <esdemetrion@gmail.com>
2023-06-30 13:29:32 -0700

    Fixes null session bug on new shop form

--- app/new-shop/page.jsx
+++ app/new-shop/page.jsx
@@ -1,14 +1,11 @@
-import ShopForm from './shop-form'
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
+import ShopForm from './shop-form'
 
 export default async function Page() {
-    const supabase = createClientComponentClient({ cookies })
+    const supabase = createServerComponentClient({ cookies })
 
     const { data: { session }, } = await supabase.auth.getSession()
-    console.log('cookies: ', { cookies })
-    console.log('session: ', session)
-    console.log('user: ', session?.user)
 
     return (
         <ShopForm session={session}/>
