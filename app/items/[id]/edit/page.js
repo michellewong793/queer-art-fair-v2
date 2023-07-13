79417247a69b6b3bab85a39d4aeadd79a147ab29
Elaine Demetrion <esdemetrion@gmail.com>
2023-07-13 16:48:14 -0700

    Deletes cruft from app/items/[id]/edit/page.js

--- app/items/[id]/edit/page.js
+++ app/items/[id]/edit/page.js
@@ -1,10 +1,7 @@
 import { cookies } from "next/headers";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import ItemEditForm from './item-edit-form'
-import AccountForm from '../../../account/account-form'
 
-// need everything about the shop, put it in form fields, allow user to edit and submit updates
-// initially, only dealing with shop stuff (not shop's items)
 export default async function Page( {params} ) {
     const supabase = createServerComponentClient({ cookies })
 
