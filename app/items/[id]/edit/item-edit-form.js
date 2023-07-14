648b8fa213a831e6fe91d151db49b95784e52edc
Elaine Demetrion <esdemetrion@gmail.com>
2023-07-14 12:56:27 -0700

    Changes textarea children to defaultValue to resolve error

--- app/items/[id]/edit/item-edit-form.js
+++ app/items/[id]/edit/item-edit-form.js
@@ -134,12 +134,11 @@ export default function ItemEditForm( props ) {
                 <label>Keywords: </label>
                 <textarea
                     type='text'
+                    defaultValue={keywords.join(', ')}
                     onChange={(e) => {
                         setKeywords((e.target.value).split(/[ \n]*,[ \n]*|[, ]*\n[, ]*/).filter(keyword => keyword.match('[A-Za-z]'))); //keywords must contain letters
                     }}
-                    >
-                    {keywords.join(', ')}
-                </textarea>
+                />
                 <p>Your keywords:{" "}
                     {keywords.map(keyword => {
                         return(
