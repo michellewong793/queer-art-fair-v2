8bbfdbc95021082ba8b34ce3f07be2b9fceeae4e
Elaine Demetrion <esdemetrion@gmail.com>
2023-07-13 17:23:39 -0700

    Fixes shop editing form so shops can be edited, and edited only by their owners

--- app/shops/[shop]/edit/page.jsx
+++ app/shops/[shop]/edit/page.jsx
@@ -1,17 +1,19 @@
 import { cookies } from "next/headers";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import ShopEditForm from './shop-edit-form'
+import ShopEditForm from "./shop-edit-form";
 
-// need everything about the shop, put it in form fields, allow user to edit and submit updates
-// initially, only dealing with shop stuff (not shop's items)
 export default async function Page( {params} ) {
-    const supabaseServer = createServerComponentClient({ cookies })
+    const supabase = createServerComponentClient({ cookies })
+
     const {
         data: { session },
-    } = await supabaseServer.auth.getSession()
-
+    } = await supabase.auth.getSession()
+    
+    
     return (
+        <>
         <ShopEditForm params={params} session={session}/>
+        </>
     )
     
 }
--- app/shops/[shop]/edit/shop-edit-form.jsx
+++ app/shops/[shop]/edit/shop-edit-form.jsx
@@ -1,55 +1,53 @@
 'use client'
-import { useState, useEffect } from "react";
-import { redirect } from "next/navigation";
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
-export default function ShopEditForm({ params }, { session }) {
-    const user = session?.user
-    // redirects non-authenticated users so they can't edit a shop
-    if (!user) redirect('/shops/'+params.shop)
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
+import { useState, useEffect } from "react"
+import { useRouter } from "next/navigation";
 
+export default function ShopEditForm( props ) {
     const supabase = createClientComponentClient()
+    const user = props.session?.user
+    const router = useRouter()
+
+    const nameFromUrl = (props.params?.shop).split('_').join(' ')
     const [name, setName] = useState()
     const [description, setDescription] = useState()
     const [instagram, setInstagram] = useState()
     const [venmo, setVenmo] = useState()
-    const [id, setId] = useState();
+    const [id, setId] = useState()
     const [formError, setFormError] = useState()
-    const [updated, setUpdated] = useState(false);
+    const [verifiedShopowner, setVerifiedShopowner] = useState(false)
 
-    useEffect(() => {
-        async function getShop() {
-            const shopName = (params.shop).split('_').join(' ')
-            let { data, error } = await supabase
-                .from('shops')
-                .select()
-                .eq('name', shopName)
-                .single()
-            
-            if (error) {
-                console.warn(error)
-                return
-            }
-            if (data) {
-                // redirects users if it turns out they aren't the shop owner
-                if (!user.id == data.owner_id) redirect('/shops/'+params.shop);
-
-                setId(data.id)
-                setName(data.name)
-                setDescription(data.description)
-                setInstagram(data.instagram)
-                setVenmo(data.venmo)
-            }
+    // gets the shop and verifies that the user is the shopowner
+    const getShop = async() => {
+        let { data, error } = await supabase
+            .from('shops')
+            .select()
+            .eq('name', nameFromUrl)
+            .single()
+        
+        if (error) {
+            console.warn(error)
+            return
         }
-        getShop()
-    }, [])
+        if (data && user?.id !== data.owner_id) {
+            router.replace('/shops/'+props.params.shop)
+        }
+        else if (data) {
+            setVerifiedShopowner(true)
+            setId(data.id)
+            setName(data.name)
+            setDescription(data.description)
+            setInstagram(data.instagram)
+            setVenmo(data.venmo)
+        }
+    }
 
     async function updateShop(e) {
         e.preventDefault()
         if (!name) { setFormError("Your shop must have a unique name."); return; }
         else if (!description) { setFormError("Your shop must have a description."); return; }
         else if (!venmo) { setFormError("Your shop must have a venmo setup so you can receive payments."); return; }
-        else { setFormError(null) }
 
         let { error } = await supabase
             .from('shops')
@@ -64,51 +62,52 @@ export default function ShopEditForm({ params }, { session }) {
         if (error) {
             setFormError("Your shop could not be updated. Error: ", error.message)
         } else {
-            setUpdated(true);
+            router.push('/shops/'+name.split(' ').join('_'))
         }
     }
 
+    // retrieve the shop, check if the shopowner is logged in
     useEffect(() => {
-        if (updated) {
-            redirect('/shops/'+name.split(' ').join('_'))
-        }
-    }, [updated])
+        getShop()
+    }, [])
 
     return (
         <>
-        <form onSubmit={(e) => updateShop(e)}>
-            <label>Name: </label>
-            <input
-                type="text"
-                value={name || ''}
-                onChange={(e) => setName(e.target.value)}
-            />
+        {verifiedShopowner ?
+            <form onSubmit={(e) => updateShop(e)}>
+                <label>Name: </label>
+                <input
+                    type="text"
+                    value={name || ''}
+                    onChange={(e) => setName(e.target.value)}
+                />
 
-            <label>Description: </label>
-            <textarea
-                type="text"
-                value={description || ''}
-                onChange={(e) => setDescription(e.target.value)}
-            />
+                <label>Description: </label>
+                <textarea
+                    type="text"
+                    value={description || ''}
+                    onChange={(e) => setDescription(e.target.value)}
+                />
 
-            <label>Instagram: </label>
-            <input
-                type="text"
-                value={instagram || ''}
-                onChange={(e) => setInstagram(e.target.value)}
-            />
+                <label>Instagram: </label>
+                <input
+                    type="text"
+                    value={instagram || ''}
+                    onChange={(e) => setInstagram(e.target.value)}
+                />
 
-            <label>Venmo: </label>
-            <input
-                type="text"
-                value={venmo || ''}
-                onChange={(e) => setVenmo(e.target.value)}
-            />
+                <label>Venmo: </label>
+                <input
+                    type="text"
+                    value={venmo || ''}
+                    onChange={(e) => setVenmo(e.target.value)}
+                />
 
-            <button type="submit">Update</button>
-        </form>
+                <button type="submit">Update</button>
 
-        {formError && <p>{formError}</p>}
+                {formError && <p>{formError}</p>}
+            </form> : <div></div>        
+        }
         </>
     )
 }
