b54f8f0e7d62f3416674f6988b498b9fadba9718
CDarling25 <135234079+CDarling25@users.noreply.github.com>
2023-07-19 12:15:55 -0500

    started the styling for the login page

--- app/login/page.js
+++ app/login/page.js
@@ -3,13 +3,35 @@
 import signInWithGoogle from "./signInWithGoogle"
 import signInWithMagicLink from "./signInWithMagicLink"
 import { useState } from "react"
+import Spacer from "../../components/Spacer";
+import Navigation from "../../components/Navigation";
+import Strawberry from "../../components/Strawberry";
+import HeaderDecoration from "../../components/HeaderDecoration";
+import Logo from "../../components/Logo";
+import Subheader from "../../components/Subheader";
+import pageStyles from "../../components/pages.module.css";
+import Footer from "../../components/Footer";
+import style from './login.module.css';
 
 export default function Login() {
     const [email, setEmail] = useState();
     return(
         <>
-        <h3>Login</h3>
-        <button onClick={() => signInWithGoogle()}>Sign in with Google</button>
+        <HeaderDecoration />
+        <Logo />
+        <Subheader />
+        <Navigation />
+        <Strawberry heading = {"LOGIN"} showMediumStrawberry = {"none"}/>
+        <Spacer height = {4}/>
+
+        <div className = {style.container}>
+            <button className = {style.signInGoogle} onClick={() => signInWithGoogle()}>
+                <div className = {style.signInText}>
+                    Sign in with Google
+                </div>
+            </button>
+        </div>
+        
 
         <br/>
         <input placeholder='email' onChange={(e) => setEmail(e.target.value)}></input>
@@ -17,7 +39,7 @@ export default function Login() {
             Sign in with magic link
         </button>
         
-        
+        <Footer />
         </>
         
     )
