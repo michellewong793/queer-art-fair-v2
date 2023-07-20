1463ac5cb873491e9d5caa605d38bf2c7bb25e43
CDarling25 <135234079+CDarling25@users.noreply.github.com>
2023-07-20 11:01:48 -0500

    finished the styling for desktop and mobile

--- app/login/page.js
+++ app/login/page.js
@@ -9,38 +9,42 @@ import Strawberry from "../../components/Strawberry";
 import HeaderDecoration from "../../components/HeaderDecoration";
 import Logo from "../../components/Logo";
 import Subheader from "../../components/Subheader";
-import pageStyles from "../../components/pages.module.css";
 import Footer from "../../components/Footer";
 import style from './login.module.css';
+import Styles from "../../components/Theme";
 
 export default function Login() {
     const [email, setEmail] = useState();
     return(
-        <>
+        <div style = {Styles.body} className = {style.bodyBackground}>
         <HeaderDecoration />
         <Logo />
         <Subheader />
         <Navigation />
         <Strawberry heading = {"LOGIN"} showMediumStrawberry = {"none"}/>
         <Spacer height = {4}/>
-
-        <div className = {style.container}>
+        <div className = {style.columnContainer}>
             <button className = {style.signInGoogle} onClick={() => signInWithGoogle()}>
+                <img className = {style.googleLogo} src = {"../../googleLogo.svg"}/>
+                <Spacer width = {1}/>
                 <div className = {style.signInText}>
                     Sign in with Google
                 </div>
             </button>
+            <Spacer height = {2} />
+            <div className = {style.bold}>OR</div>
+            <Spacer height = {2}/>
+            <input placeholder='enter your email...' className = {style.enterEmail} onChange={(e) => setEmail(e.target.value)}></input>
+            <Spacer height = {1}/>
+            <button className = {style.emailSignIn} onClick={() => signInWithMagicLink({email: email})}>
+                <div className = {style.signInText}>
+                    Sign in with magic link
+                </div>
+            </button>
         </div>
         
-
-        <br/>
-        <input placeholder='email' onChange={(e) => setEmail(e.target.value)}></input>
-        <button onClick={() => signInWithMagicLink({email: email})}>
-            Sign in with magic link
-        </button>
-        
         <Footer />
-        </>
+        </div>
         
     )
 }
