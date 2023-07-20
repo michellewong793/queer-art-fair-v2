4aa26e6e7d8a5ba0a7dd8314e68b4e9ea43168a6
Elaine Demetrion <esdemetrion@gmail.com>
2023-07-19 18:02:10 -0700

    Creates item card for shop page

---
+++ app/components/ItemCard.js
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+import styles from './ItemCard.module.css'
+
+// props = item
+const ItemCard = ({item}) => {
+    
+    return (
+        
+        <div className={styles.wrapper} title={item?.name}>
+            <Link className={styles.link} href={'/items/'+item?.id}>
+                <img className={styles.image} src={item?.image_urls[0]} />
+                <div className={styles.textWrapper}>
+                    <p className={styles.itemName}>{item?.name}</p>
+                    <p className={styles.itemPrice}>${item?.price}</p>
+                </div>
+            </Link>
+        </div>
+    )
+}
+
+export default ItemCard
