092a97553f807e0adc94d960abf41de95982c2e3
Elaine Demetrion <esdemetrion@gmail.com>
2023-07-21 14:36:50 -0700

    Adds basic styling and elements to new shop page

--- app/shops/new/page.jsx
+++ app/shops/new/page.jsx
@@ -1,5 +1,13 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
+import styles from "./Page.module.css";
+import theme from "../../../components/Theme";
+import Navigation from "../../../components/Navigation";
+import Strawberry from "../../../components/Strawberry";
+import Logo from "../../../components/Logo";
+import Subheader from "../../../components/Subheader";
+import HeaderDecoration from "../../../components/HeaderDecoration";
+import Footer from "../../../components/Footer";
 import ShopForm from './shop-form'
 
 export default async function Page() {
@@ -7,7 +15,18 @@ export default async function Page() {
 
     const { data: { session }, } = await supabase.auth.getSession()
 
-    return (
-        <ShopForm session={session}/>
+    return(
+        <div style={theme.body} className={styles.background}>
+        <HeaderDecoration/>
+        <Logo />
+        <Subheader />
+        <Navigation />
+        <Strawberry heading="NEW ITEM" showLargeStrawberry='none' />
+        <div className={styles.content}>
+            <ShopForm session={session}/>
+        </div>
+        
+        <Footer/>
+        </div>
     )
 }
