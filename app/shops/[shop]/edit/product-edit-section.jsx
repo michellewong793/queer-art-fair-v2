69a764d4ec3dec87a1901723e55d67328599623c
Elaine Demetrion <esdemetrion@gmail.com>
2023-07-25 13:08:13 -0700

    Adds New Item Card to the product edit section

--- app/shops/[shop]/edit/product-edit-section.jsx
+++ app/shops/[shop]/edit/product-edit-section.jsx
@@ -1,11 +1,13 @@
 import ItemCard from "../../../components/ItemCard"
 import styles from "./ProductEditSection.module.css"
+import NewItemCard from "./NewItemCard"
 
 export default async function ProductEditSection({items}) {    
     return (
         <>
         <h3>Your Products</h3>
         <div className={styles.products}>
+            <NewItemCard />
             {items?.map(item => (
                 <ItemCard item={item} edit={true}/>
             ))}
