638a473d7897e34b112e5e79a394345927961bb2
Elaine Demetrion <esdemetrion@gmail.com>
2023-07-25 12:12:07 -0700

    Item card now optionally links to the item edit page

--- app/components/ItemCard.js
+++ app/components/ItemCard.js
@@ -2,17 +2,20 @@ import Link from 'next/link'
 import styles from './ItemCard.module.css'
 
 // props = item
-const ItemCard = ({item}) => {
+const ItemCard = (props) => {
+    const editUrl = '/items/'+props?.item?.id+'/edit'
+    const viewUrl = '/items/'+props?.item?.id
+    const url = props?.edit ? editUrl : viewUrl
     
     return (
         
-        <div className={styles.wrapper} title={item?.name}>
-            <Link className={styles.link} href={'/items/'+item?.id}>
-                <img className={styles.image} src={item?.image_urls[0]} 
-                />
+        <div className={styles.wrapper} title={props?.item?.name}>
+            <Link className={styles.link} href={url}>
+                <img className={styles.image} src={props?.item?.image_urls[0]} />
+                <img className={props?.edit ? styles.edit : styles.hidden} src={'/PencilEditIcon.svg'}/>
                 <div className={styles.textWrapper}>
-                    <p className={styles.itemName}>{item?.name}</p>
-                    <p className={styles.itemPrice}>${item?.price}</p>
+                    <p className={styles.itemName}>{props?.item?.name}</p>
+                    <p className={styles.itemPrice}>${props?.item?.price}</p>
                 </div>
             </Link>
         </div>
