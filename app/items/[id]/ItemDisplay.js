a1325cdcfafd2ad4ed2da970007066f2e1585799
Elaine Demetrion <esdemetrion@gmail.com>
2023-07-26 13:05:08 -0700

    Fixes shop link

--- app/items/[id]/ItemDisplay.js
+++ app/items/[id]/ItemDisplay.js
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import styles from './ItemDisplay.module.css'
 import Button from '../../../components/Button'
+import ImageSlideshow from './ImageSlideshow'
 
 // props: item, shop
 export default function ItemDisplay(props) {
@@ -8,12 +9,12 @@ export default function ItemDisplay(props) {
     const shop = props?.shop
     return (
         <div className={styles.wrapper}>
-        {/* slideshow */}
+            <ImageSlideshow imageUrls={item.image_urls}/>
             <div className={styles.namePriceWrapper}>
                 <h3>{item?.name}</h3>
                 <h3>${item?.price}</h3>
             </div>
-            <p className={styles.shop}>Sold by <Link href={'/shops/'+shop?.name}>{shop?.name}</Link></p>
+            <p className={styles.shop}>Sold by <Link href={'/shops/'+shop?.name.split(' ').join('_')}>{shop?.name}</Link></p>
 
             <h3>Details</h3>
             <p className={styles.description}>{item?.description}</p>
