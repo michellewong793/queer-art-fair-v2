0270b6ce7d69518f3c0f5e7d28e95a51cab6d289
Elaine Demetrion <esdemetrion@gmail.com>
2023-07-27 09:42:22 -0700

    Basic styling for new item page

--- app/shops/[shop]/new-item/page.jsx
+++ app/shops/[shop]/new-item/page.jsx
@@ -4,6 +4,13 @@ import ItemForm from './ItemForm'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
+import theme from "../../../../components/Theme";
+import HeaderDecoration from '../../../../components/HeaderDecoration';
+import Logo from "../../../../components/Logo";
+import Subheader from "../../../../components/Subheader";
+import Navigation from "../../../../components/Navigation";
+import Footer from "../../../../components/Footer";
+import styles from "./page.module.css"
 
 export default async function Page({params}) {
     const supabase = createServerComponentClient({ cookies })
@@ -36,7 +43,16 @@ export default async function Page({params}) {
 
     return (
         <>
-        {shop && <ItemForm shop={shop}/>}
+        <div style={theme.body} className={styles.background}>
+            <HeaderDecoration />
+            <Logo />
+            <Subheader />
+            <Navigation />
+            <div className={styles.content}>
+                {shop && <ItemForm shop={shop}/>}
+            </div>
+            <Footer />
+        </div>
         </>
         
     )
