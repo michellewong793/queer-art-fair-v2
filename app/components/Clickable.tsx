d4bf2c26adbf669ecf66c0422919912088ead7a5
Elaine Demetrion <esdemetrion@gmail.com>
2023-07-27 23:25:34 -0700

    adds general Clickable component, which has styling that could be used by many clickable components

---
+++ app/components/Clickable.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from "react"
+import styles from './Clickable.module.css'
+
+type ClickableProps = {
+    className?,
+    children: ReactNode;
+}
+
+const Clickable: React.FC<ClickableProps> = ({
+    className,
+    children
+}) => {
+    return (
+        <div className={styles.clickable + ' ' + className}>
+            {children}
+        </div>
+    )
+}
