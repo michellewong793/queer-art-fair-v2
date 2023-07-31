565b94c0a985b5fd7a05ecb92708121f1147491a
Elaine Demetrion <esdemetrion@gmail.com>
2023-07-31 14:27:54 -0700

    Creates a deletable image component to use when creating/editing item images

---
+++ app/components/DeletableImage.js
@@ -0,0 +1,25 @@
+import Clickable from './Clickable'
+import styles from './DeletableImage.module.css'
+
+export default function DeletableImage ( props ) {
+    const handleOnClick = (event) => {
+        if (typeof props?.deleteFunction === 'function') {
+            props.deleteFunction()
+        }
+    }
+
+    return (
+        <Clickable className={styles.imageWrapper}>
+            <img 
+                className={styles.image}
+                src={props?.imageUrl} 
+                />
+
+            <img
+                className={styles.deleteButton}
+                src='/TrashIcon.svg'
+                onClick={handleOnClick}
+            />                
+        </Clickable>
+    )
+}
