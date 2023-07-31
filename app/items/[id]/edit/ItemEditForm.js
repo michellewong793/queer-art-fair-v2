dcc5b89594f351435e2d52030683f67f9fdcd9ba
Elaine Demetrion <esdemetrion@gmail.com>
2023-07-31 11:26:47 -0700

    Adds errors to item edit form

--- app/items/[id]/edit/ItemEditForm.js
+++ app/items/[id]/edit/ItemEditForm.js
@@ -1,28 +1,79 @@
 'use client'
-
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
 import Label from '../../../components/forms/Label'
 import Input from '../../../components/forms/Input'
 import styles from './ItemEditForm.module.css'
 import { useState } from 'react'
+// TODO: other errors, non numeric quantities/prices
 
 export default function ItemEditForm( props ) {
     const item = props?.item
+    const supabase = createClientComponentClient()
 
     const [name, setName] = useState(item?.name)
     const [description, setDescription] = useState(item?.description)
     const [price, setPrice] = useState(item?.price)
     // TODO: how to deal with quantity without screwing up orders in progress
     const [quantity, setQuantity] = useState(item?.quantity)
-    const [keywords, setKeywords] = useState(item?.keywords)
+    const [keywords, setKeywords] = useState(item?.keywords || [])
+    const [images, setImages] = useState([])
 
+    const [nameError, setNameError] = useState(null)
+    const [descriptionError, setDescriptionError] = useState(null)
+    const [priceError, setPriceError] = useState(null)
+    const [quantityError, setQuantityError] = useState(null)
+    const [keywordError, setKeywordError] = useState(null)
+    const [imageError, setImageError] = useState(null)
+
+    function checkFields() {
+        let error = false;
+        if (!name) {
+            setNameError('*Item name is required.')
+            error = true;
+        } else { setNameError(null) }
+
+        if (!description) {
+            setDescriptionError('*Description is required.')
+            error = true;
+        } else { setDescriptionError(null) }
+
+        if (!price) {
+            setPriceError('*Price is required.')
+            error = true;
+        } else { setPriceError(null) }
+    
+        if (!quantity) {
+            setQuantityError('*Quantity is required.')
+            error = true;
+        } else { setQuantityError(null); }
+
+        if (!keywords || keywords?.length == 0) {
+            setKeywordError('*Keywords are required.')
+            error = true;
+        }
+
+        if (!images || images?.length == 0) {
+            setImageError('*Images are required.')
+            error = true;
+        }
+
+        return error
+    }
+
+    async function updateItem(e) {
+        e.preventDefault()
+        if (checkFields()) return
+
+    }
     return (
-        <form>
+        <form onSubmit={(e) => updateItem(e)}>
             <Label><strong>Name*</strong></Label>
             <Input
                 className={styles.input}
                 type='text'
                 value={name}
                 onChange={(data) => setName(data.value)}
+                error={nameError}
             />
 
             <Label><strong>Description*</strong></Label>
@@ -31,6 +82,7 @@ export default function ItemEditForm( props ) {
                 type='textarea'
                 defaultValue={description}
                 onChange={(data) => setDescription(data.value)}
+                error={descriptionError}
             /> 
 
             <Label><strong>Price*</strong></Label>
@@ -40,9 +92,8 @@ export default function ItemEditForm( props ) {
                 min='0.01'
                 step='0.01'
                 value={price}
-                onChange={(data) => {
-                    setPrice(data.value);
-                }}
+                onChange={(data) => { setPrice(data.value); }}
+                error={priceError}
             />  
 
             <Label><strong>Quantity*</strong></Label>
@@ -51,9 +102,8 @@ export default function ItemEditForm( props ) {
                 type='number'
                 min='1'
                 value={quantity}
-                onChange={(data) => {
-                    setQuantity(data.value);
-                }}
+                onChange={(data) => { setQuantity(data.value); }}
+                error={quantityError}
             />  
 
             <Label><strong>Keywords*</strong></Label>
@@ -64,12 +114,15 @@ export default function ItemEditForm( props ) {
                 onChange={(data) => {
                     setKeywords((data.value).split(/[ \n]*,[ \n]*|[, ]*\n[, ]*/).filter(keyword => keyword.match('[A-Za-z]'))); //keywords must contain letters
                 }}
+                error={keywordError}
             />
 
             <Label><strong>Images*</strong></Label>
             <Input
                 className={styles.input + ' ' + styles.fileInput}
                 type='file'
+                accept="image/png, image/jpeg, image/jpg"
+                error={imageError}
             />
 
             <Input 
