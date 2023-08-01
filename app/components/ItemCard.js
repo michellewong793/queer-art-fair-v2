f6725abeb8cce248c5a57083848ceb2fff598baa
Elaine Demetrion <esdemetrion@gmail.com>
2023-08-01 15:51:50 -0700

    Changes the item card component to use the newly created card component

--- app/components/ItemCard.js
+++ app/components/ItemCard.js
@@ -1,6 +1,5 @@
-import Link from 'next/link'
 import styles from './ItemCard.module.css'
-import Clickable from './Clickable'
+import Card from './Card'
 
 // props = item
 const ItemCard = (props) => {
@@ -9,18 +8,15 @@ const ItemCard = (props) => {
     const url = props?.edit ? editUrl : viewUrl
     
     return (
-        <Clickable>
-        <div className={styles.wrapper} title={props?.item?.name}>
-            <Link className={styles.link} href={url} rel="noopener noreferrer" target="_blank">
-                <img className={styles.image} src={props?.item?.image_urls[0]} />
-                <img className={props?.edit ? styles.editIcon : styles.hidden} src={'/PencilEditIcon.svg'}/>
-                <div className={styles.textWrapper}>
-                    <p className={styles.itemName}>{props?.item?.name}</p>
-                    <p className={styles.itemPrice}>${props?.item?.price}</p>
-                </div>
-            </Link>
-        </div>
-        </Clickable>
+        <Card
+        image={props?.item?.image_urls[0]}
+        url={viewUrl}
+        edit={props?.edit}
+        editUrl={editUrl}
+        >
+            <p className={styles.itemName}>{props?.item?.name}</p>
+            <p className={styles.itemPrice}>${props?.item?.price}</p>
+        </Card>
     )
 }
 
