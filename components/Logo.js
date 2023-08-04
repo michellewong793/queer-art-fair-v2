8864450ff1a46e101ff91a79deeb00b90fa6726b
Elaine Demetrion <esdemetrion@gmail.com>
2023-08-04 12:19:33 -0700

    Adds alt text to the rest of the images

--- components/Logo.js
+++ components/Logo.js
@@ -2,7 +2,7 @@ import Styles from './Logo.module.css'
 
 const Logo = (props) => {
     return (
-        <img className={Styles.logoImg} src='/./QAFLogoOrangePuffy.png'></img>
+        <img className={Styles.logoImg} src='/./QAFLogoOrangePuffy.png' alt='Queer Art Faire logo, an inflatable heart-shaped fruit with water droplets and the letters Q A F written inside'/>
     )
 }
 
