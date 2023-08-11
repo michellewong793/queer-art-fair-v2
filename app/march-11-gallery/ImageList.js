4984a62377aa1ee845ce6eeda808c282c29e88fc
Elaine Demetrion <esdemetrion@gmail.com>
2023-08-11 01:13:41 -0700

    Updates march 11 gallery page accessibility

---
+++ app/march-11-gallery/ImageList.js
@@ -0,0 +1,72 @@
+const ImageList = [
+    {
+        src: '/photos/P3115740-min.JPG',
+        alt: 'Multiple vendors'
+    },
+    {
+        src: '/photos/P3115759-min.JPG',
+        alt: 'Two vendors smiling and displaying their paintings'
+    },
+    {
+        src: '/photos/P3115806-min.JPG',
+        alt: 'Two attendees and the jewelry they are selling'
+    },
+    {
+        src: '/photos/P3115854-min.JPG',
+        alt: 'Two attendees and the jewelry they are selling'
+    },
+    {
+        src: '/photos/P3115914-min.JPG',
+        alt: 'Birds eye view of the fair, showing four columns of vendors and their art'
+    },
+    {
+        src: '/photos/P3115925-min.JPG',
+        alt: 'Birds eye view of the fair, showing four rows of vendors along with vendors along the perimeter of the room'
+    },
+    {
+        src: '/photos/P3115734-min.JPG',
+        alt: 'One attendee smiling and wearing black gloves'
+    },
+    {
+        src: '/photos/P3115753-min.JPG',
+        alt: 'Cake Therapy and Kuya Studio showing off their display of cupcakes and art'
+    },
+    {
+        src: '/photos/P3115837-min.JPG',
+        alt: 'Feeling Blue plant and sky themed handmade crafts'
+    },
+    {
+        src: '/photos/P3115887-min.JPG',
+        alt: 'Two vendors selling jewelry talking to another attendee'
+    },
+    {
+        src: '/photos/P3115816-min.JPG',
+        alt: 'Vendors selling prints, one of them holding a cute dog'
+    },
+    {
+        src: '/photos/P3115882-min.JPG',
+        alt: 'A vendor sitting at a table of their prints'
+    },
+    {
+        src: '/photos/P3115892-min.JPG',
+        alt: 'Two vendors smiling and sitting at a table of artwork'
+    },
+    {
+        src: '/photos/P3115790-min.JPG',
+        alt: 'A vendor showing off their paintings'
+    },
+    {
+        src: '/photos/P3115767-min.JPG',
+        alt: 'A vendor smiling behind their stand, displaying a variety of stickers'
+    },
+    {
+        src: '/photos/P3115745-min.JPG',
+        alt: 'A vendor smiling behind their stand, displaying knit items and jewelry'
+    },
+    {
+        src: '/photos/P3115786-min.JPG',
+        alt: 'Dirtboy ceramics displaying a collection of pottery'
+    }
+]
+
+export default ImageList
--- app/march-11-gallery/page.js
+++ app/march-11-gallery/page.js
@@ -8,6 +8,7 @@ import Subheader from "../../components/Subheader";
 import pageStyles from "../../components/pages.module.css";
 import Footer from "../../components/Footer";
 import GalleryPics from "../../components/GalleryPics";
+import ImageList from './ImageList'
 
 export default function Gallery() {
     return (
@@ -18,15 +19,20 @@ export default function Gallery() {
             <Navigation />
             <Strawberry heading = {"PHOTO GALLERY"} showLargeStrawberry = {"none"}/>
             <Spacer height = {2}/>
-            <GalleryPics imagePath1 = {"url('/photos/P3115740-min.JPG')"} imagePath2 = {"url('/photos/P3115759-min.JPG')"}/>
-            <GalleryPics imagePath1 = {"url('/photos/P3115806-min.JPG')"} imagePath2 = {"url('/photos/P3115854-min.JPG')"}/>
-            <GalleryPics imagePath1 = {"url('/photos/P3115914-min.JPG')"} imagePath2 = {"url('/photos/P3115925-min.JPG')"}/>
-            <GalleryPics imagePath2 = {"url('/photos/P3115734-min.JPG')"} imagePath1 = {"url('/photos/P3115753-min.JPG')"}/>
-            <GalleryPics imagePath1 = {"url('/photos/P3115795-min.JPG')"} imagePath2 = {"url('/photos/P3115837-min.JPG')"}/>
-            <GalleryPics imagePath1 = {"url('/photos/P3115887-min.JPG')"} imagePath2 = {"url('/photos/P3115816-min.JPG')"}/>
-            <GalleryPics imagePath1 = {"url('/photos/P3115882-min.JPG')"} imagePath2 = {"url('/photos/P3115892-min.JPG')"}/>
-            <GalleryPics imagePath1 = {"url('/photos/P3115790-min.JPG')"} imagePath2 = {"url('/photos/P3115767-min.JPG')"}/>
-            <GalleryPics imagePath1 = {"url('/photos/P3115745-min.JPG')"} imagePath2 = {"url('/photos/P3115786-min.JPG')"}/>
+            {ImageList.map((image, k) => { 
+                if (k%2 === 1) return // skip every odd number
+                let image2;
+
+                // no image 2 if it's past the last image
+                if (k+1 >= ImageList.length){
+                    image2=false
+                } else {
+                    image2 = ImageList[k+1]
+                }
+                return (
+                    <GalleryPics imagePath1={image.src} alt1={image.alt} imagePath2={image2?.src} alt2={image2?.alt} />
+                )
+            })}
             <Footer />
         </div>
     );
